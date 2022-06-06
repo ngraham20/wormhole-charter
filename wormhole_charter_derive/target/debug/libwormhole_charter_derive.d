8280d37ea09983b239eab2e67fb0133b8e59e456
Nathaniel Graham <nathaniel.graham@protonmail.com>
2022-06-05 20:28:55 -0600

    removed extra files

--- wormhole_charter_derive/target/debug/libwormhole_charter_derive.d
+++
@@ -1,1 +0,0 @@
-/home/nathanielgraham/repos/wormhole-charter/wormhole_charter_derive/target/debug/libwormhole_charter_derive.so: /home/nathanielgraham/repos/wormhole-charter/wormhole_charter_derive/src/lib.rs
